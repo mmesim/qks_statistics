030e5974499e0eac0942ef1bf976f06456c781bc
mmesim <mmesim@gmail.com>
2020-09-09 10:08:39 -0600

    "Depth Distribution"

---
+++ depth_distr.m
@@ -0,0 +1,15 @@
+function []=depth_distr(a)
+% Plain simple histogram of depths
+
+depth=a(:,9);
+
+f1=figure;
+histogram(depth,min(depth):0.5:max(depth))
+xlabel('Depth (km)')
+ylabel('N obs')
+set(gca,'FontSize',14,'FontName','Helvetica')
+
+saveas(f1,'Depth_distr.eps','eps')
+
+close(f1)
+end
