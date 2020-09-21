8a53fb5f69614fbdb0a948852bd756bbbb8f425d
mmesim <mmesim@gmail.com>
2020-09-21 16:31:50 -0600

    "Update"

--- my_qc_catalog.m
+++ my_qc_catalog.m
@@ -3,3 +3,22 @@
 clear;clc;close all
 
 %% 00. load catalog
+filename='Utah.txt';
+a=read_cat(filename);
+
+%% 01. fix magnitudes -- round to 0.1
+ar=mag_rounding(a);
+
+%% 02. Depth distribution
+depth_distr(ar);
+
+%% 03. Cum Vs. Time
+cum_time(ar);
+%% 04. Mag Vs Time
+mag_time(ar);
+%% 05. Latitude Vs. Time
+lat_time(ar);
+%% 06. Spatial Map
+map_xy(ar)
+%% 08. GOF [entire catalog]
+mc_gof(ar);
