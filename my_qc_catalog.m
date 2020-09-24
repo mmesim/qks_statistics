f2b83d05596e0b17f76f09793ce5a37e69ea6a1b
mmesim <mmesim@gmail.com>
2020-09-24 10:27:00 -0600

    "pause"

--- my_qc_catalog.m
+++ my_qc_catalog.m
@@ -3,21 +3,26 @@
 clear;clc;close all
 
 %% 00. load catalog
-filename='Utah.txt';
+filename='UtahMlMc.txt';
 a=read_cat(filename);
 
 %% 01. fix magnitudes -- round to 0.1
 ar=mag_rounding(a);
-
+pause
 %% 02. Depth distribution
 depth_distr(ar);
+pause
 %% 03. Cum Vs. Time
 cum_time(ar);
+pause
 %% 04. Mag Vs Time
 mag_time(ar);
+pause
 %% 05. Latitude Vs. Time
 lat_time(ar);
+pause
 %% 06. Spatial Map
 map_xy(ar)
+pause
 %% 07. GOF [entire catalog]
 MCgft(ar);
